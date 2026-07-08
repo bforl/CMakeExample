a0b24f15c12c4a22497edb9d5aac942ed977eb5e
bforl <forltiko@gmail.com>
2026-07-08 16:50:21 +0100

    add vcpkg

--- CMakeProject1.cpp
+++ CMakeProject1.cpp
@@ -1,14 +1,18 @@
-﻿// CMakeProject1.cpp : Defines the entry point for the application.
-//
-
-#include "CMakeProject1.h"
+﻿#include "CMakeProject1.h"
 #include <cstdint>
-#include <cstdints>
+
+#include <fmt/core.h>
 
 using namespace std;
 
 int main()
 {
+	std::vector<int> values = { 1, 2, 3, 4, 5 };
+
+	fmt::print("Hello from vcpkg!\n");
+	fmt::print("Vector contains {} elements.\n", values.size());
+
 	cout << "Hello CMake." << endl;
+	
 	return 0;
 }
